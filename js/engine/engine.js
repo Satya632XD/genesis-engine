297675aa2fae750c82b2ae8e949ee8e424827b0f
Satya632XD <kingadheerafansatya@gmail.com>
2026-06-28 13:07:16 +0530

    Update engine.js

--- js/engine/engine.js
+++ js/engine/engine.js
@@ -1,146 +1,39 @@
 import { Renderer }
 from "./renderer.js";
 
-class Engine{
+class Engine {
 
-constructor(){
+    constructor() {
 
-this.canvas =
-document.getElementById(
-"gameCanvas"
-);
+        this.canvas =
+        document.getElementById("gameCanvas");
 
-this.loadingFill =
-document.getElementById(
-"loadingFill"
-);
+        this.seed =
+        Math.floor(Math.random() * 999999999);
 
-this.loadingText =
-document.getElementById(
-"loadingText"
-);
+        this.renderer =
+        new Renderer(this.canvas, this.seed);
 
-this.renderer =
-new Renderer(this.canvas);
+        this.lastTime = 0;
 
-this.lastTime=0;
+        this.loop = this.loop.bind(this);
 
-this.delta=0;
+        requestAnimationFrame(this.loop);
 
-this.frames=0;
+    }
 
-this.fps=0;
+    loop(time) {
 
-this.fpsTimer=0;
+        const delta =
+        (time - this.lastTime) / 1000;
 
-this.seed=Math.floor(
+        this.lastTime = time;
 
-Math.random()*999999999
+        this.renderer.render(delta);
 
-);
+        requestAnimationFrame(this.loop);
 
-document
-.getElementById("seed")
-.innerHTML=
-"Seed : "+this.seed;
-
-this.initialize();
-
-}
-
-async initialize(){
-
-await this.fakeLoad(
-15,
-"Starting Renderer..."
-);
-
-await this.fakeLoad(
-35,
-"Preparing Graphics..."
-);
-
-await this.fakeLoad(
-55,
-"Loading Engine..."
-);
-
-await this.fakeLoad(
-80,
-"Creating World..."
-);
-
-await this.fakeLoad(
-100,
-"Finished"
-);
-
-document
-.getElementById(
-"loadingScreen"
-).style.display="none";
-
-requestAnimationFrame(
-
-this.loop.bind(this)
-
-);
-
-}
-
-fakeLoad(percent,text){
-
-return new Promise(resolve=>{
-
-this.loadingFill.style.width=
-percent+"%";
-
-this.loadingText.innerHTML=
-text;
-
-setTimeout(resolve,300);
-
-});
-
-}
-
-loop(time){
-
-this.delta=
-(time-this.lastTime)/1000;
-
-this.lastTime=time;
-
-this.frames++;
-
-this.fpsTimer+=this.delta;
-
-if(this.fpsTimer>=1){
-
-this.fps=this.frames;
-
-this.frames=0;
-
-this.fpsTimer=0;
-
-document
-.getElementById("fps")
-.innerHTML=
-"FPS : "+this.fps;
-
-}
-
-this.renderer.render(
-this.delta
-);
-
-requestAnimationFrame(
-
-this.loop.bind(this)
-
-);
-
-}
+    }
 
 }
 
