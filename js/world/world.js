e17a35bf3b3832d362a805ca1323d7eebc1f0198
Satya632XD <kingadheerafansatya@gmail.com>
2026-06-28 13:28:10 +0530

    Update world.js

--- js/world/world.js
+++ js/world/world.js
@@ -3,6 +3,7 @@ import { Dinosaur } from "../creatures/dinosaur.js";
 import { Food } from "./food.js";
 import { Weather } from "./weather.js";
 import { Fire } from "./fire.js";
+import { Smoke } from "./smoke.js";
 
 export class World {
 
@@ -19,13 +20,15 @@ export class World {
         this.creatures = [];
         this.foods = [];
         this.fires = [];
+        this.smokes = [];
 
         this.pendingFires = [];
+        this.pendingSmokes = [];
+
         this.isUpdating = false;
 
         this.weather = new Weather(seed);
 
-        // 🧨 burned terrain memory
         this.burnedTiles = new Set();
 
         this.generate();
@@ -63,9 +66,7 @@ export class World {
             const x = Math.floor(Math.random() * this.size);
             const y = Math.floor(Math.random() * this.size);
 
-            this.creatures.push(
-                new Dinosaur(x, y, this)
-            );
+            this.creatures.push(new Dinosaur(x, y, this));
 
         }
 
@@ -78,9 +79,7 @@ export class World {
             const x = Math.floor(Math.random() * this.size);
             const y = Math.floor(Math.random() * this.size);
 
-            this.foods.push(
-                new Food(x, y, this)
-            );
+            this.foods.push(new Food(x, y, this));
 
         }
 
@@ -125,15 +124,11 @@ export class World {
     }
 
     isBurned(x, y) {
-
         return this.burnedTiles.has(`${x},${y}`);
-
     }
 
     isWaterAt(x, y) {
-
-        const h = this.getHeight(Math.floor(x), Math.floor(y));
-        return h < 0.3;
+        return this.getHeight(x, y) < 0.3;
     }
 
     canBurnAt(x, y) {
@@ -163,40 +158,26 @@ export class World {
     update(delta) {
 
         this.isUpdating = true;
+
         this.pendingFires = [];
 
-        // 🌦 weather first
         this.weather.update(delta);
 
-        // 🔥 update fires before life so fire can affect creatures this frame
-        for (const fire of this.fires) {
-            fire.update(delta);
-        }
+        for (const fire of this.fires) fire.update(delta);
+        for (const food of this.foods) food.update(delta);
+        for (const creature of this.creatures) creature.update(delta);
+        for (const smoke of this.smokes) smoke.update(delta);
 
-        // 🌿 update food
-        for (const food of this.foods) {
-            food.update(delta);
-        }
+        this.fires = this.fires.filter(f => f.alive);
+        this.foods = this.foods.filter(f => f.alive);
+        this.smokes = this.smokes.filter(s => s.alive);
 
-        // 🦖 update creatures
-        for (const creature of this.creatures) {
-            creature.update(delta);
-        }
-
-        // 🧹 cleanup dead food
-        this.foods = this.foods.filter(food => food.alive);
-
-        // 🧹 cleanup dead fires
-        this.fires = this.fires.filter(fire => fire.alive);
-
-        // ➕ add spawned fires
         if (this.pendingFires.length > 0) {
             this.fires.push(...this.pendingFires);
         }
 
-        this.pendingFires = [];
         this.isUpdating = false;
 
     }
 
-}
+            }
